74de8b3c0291ad3963e9373c6c05df3327b57812
fabiozafare <84875333+fabiozafare@users.noreply.github.com>
2021-08-11 21:32:35 -0300

    https update

--- wp-login.php
+++ wp-login.php
@@ -5,11 +5,11 @@
 	<title>Acessar &lsaquo;  &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="http://webfcdigital.github.io/faberinfo/wp-includes/css/dashicons.min.css?ver=5.8" type="text/css" media="all">
-<link rel="stylesheet" id="buttons-css" href="http://webfcdigital.github.io/faberinfo/wp-includes/css/buttons.min.css?ver=5.8" type="text/css" media="all">
-<link rel="stylesheet" id="forms-css" href="http://webfcdigital.github.io/faberinfo/wp-admin/css/forms.min.css?ver=5.8" type="text/css" media="all">
-<link rel="stylesheet" id="l10n-css" href="http://webfcdigital.github.io/faberinfo/wp-admin/css/l10n.min.css?ver=5.8" type="text/css" media="all">
-<link rel="stylesheet" id="login-css" href="http://webfcdigital.github.io/faberinfo/wp-admin/css/login.min.css?ver=5.8" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://webfcdigital.github.io/faberinfo/wp-includes/css/dashicons.min.css?ver=5.8" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://webfcdigital.github.io/faberinfo/wp-includes/css/buttons.min.css?ver=5.8" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://webfcdigital.github.io/faberinfo/wp-admin/css/forms.min.css?ver=5.8" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://webfcdigital.github.io/faberinfo/wp-admin/css/l10n.min.css?ver=5.8" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://webfcdigital.github.io/faberinfo/wp-admin/css/login.min.css?ver=5.8" type="text/css" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -20,7 +20,7 @@
 		<div id="login">
 		<h1><a href="https://br.wordpress.org/">Powered by WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="http://webfcdigital.github.io/faberinfo/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://webfcdigital.github.io/faberinfo/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Nome de usuário ou endereço de e-mail</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
@@ -38,33 +38,33 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Lembrar-me</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acessar">
-									<input type="hidden" name="redirect_to" value="http://webfcdigital.github.io/faberinfo/wp-admin/">
+									<input type="hidden" name="redirect_to" value="https://webfcdigital.github.io/faberinfo/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="http://webfcdigital.github.io/faberinfo/wp-login.php?action=lostpassword">Perdeu a senha?</a>
+								<a href="https://webfcdigital.github.io/faberinfo/wp-login.php?action=lostpassword">Perdeu a senha?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="http://webfcdigital.github.io/faberinfo/">&larr; Ir para </a>		</p>
+			<a href="https://webfcdigital.github.io/faberinfo/">&larr; Ir para </a>		</p>
 			</div>
-	<script type="text/javascript" src="http://webfcdigital.github.io/faberinfo/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
-<script type="text/javascript" src="http://webfcdigital.github.io/faberinfo/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+	<script type="text/javascript" src="https://webfcdigital.github.io/faberinfo/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://webfcdigital.github.io/faberinfo/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script type="text/javascript" id="zxcvbn-async-js-extra">
 /*  */
-var _zxcvbnSettings = {"src":"http:\/\/webfcdigital.github.io\/faberinfo\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/webfcdigital.github.io\/faberinfo\/wp-includes\/js\/zxcvbn.min.js"};
 /*  */
 </script>
-<script type="text/javascript" src="http://webfcdigital.github.io/faberinfo/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script type="text/javascript" src="http://webfcdigital.github.io/faberinfo/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.7" id="regenerator-runtime-js"></script>
-<script type="text/javascript" src="http://webfcdigital.github.io/faberinfo/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script type="text/javascript" src="http://webfcdigital.github.io/faberinfo/wp-includes/js/dist/hooks.min.js?ver=a7edae857aab69d69fa10d5aef23a5de" id="wp-hooks-js"></script>
-<script type="text/javascript" src="http://webfcdigital.github.io/faberinfo/wp-includes/js/dist/i18n.min.js?ver=5f1269854226b4dd90450db411a12b79" id="wp-i18n-js"></script>
+<script type="text/javascript" src="https://webfcdigital.github.io/faberinfo/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://webfcdigital.github.io/faberinfo/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.7" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="https://webfcdigital.github.io/faberinfo/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="https://webfcdigital.github.io/faberinfo/wp-includes/js/dist/hooks.min.js?ver=a7edae857aab69d69fa10d5aef23a5de" id="wp-hooks-js"></script>
+<script type="text/javascript" src="https://webfcdigital.github.io/faberinfo/wp-includes/js/dist/i18n.min.js?ver=5f1269854226b4dd90450db411a12b79" id="wp-i18n-js"></script>
 <script type="text/javascript" id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
@@ -80,14 +80,14 @@ var pwsL10n = {"unknown":"Nível de segurança da senha desconhecido","short":"M
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2021-08-01 11:04:56+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"pt_BR"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s está obsoleta desde a versão %2$s. Use %3$s como alternativa. Considere escrever um código mais inclusivo."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
 </script>
-<script type="text/javascript" src="http://webfcdigital.github.io/faberinfo/wp-admin/js/password-strength-meter.min.js?ver=5.8" id="password-strength-meter-js"></script>
-<script type="text/javascript" src="http://webfcdigital.github.io/faberinfo/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
+<script type="text/javascript" src="https://webfcdigital.github.io/faberinfo/wp-admin/js/password-strength-meter.min.js?ver=5.8" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://webfcdigital.github.io/faberinfo/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
 <script type="text/javascript" id="wp-util-js-extra">
 /*  */
 var _wpUtilSettings = {"ajax":{"url":"\/FaberInfo\/site\/wp-admin\/admin-ajax.php"}};
 /*  */
 </script>
-<script type="text/javascript" src="http://webfcdigital.github.io/faberinfo/wp-includes/js/wp-util.min.js?ver=5.8" id="wp-util-js"></script>
+<script type="text/javascript" src="https://webfcdigital.github.io/faberinfo/wp-includes/js/wp-util.min.js?ver=5.8" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
 var userProfileL10n = {"user_id":"0","nonce":"3dd02e420b"};
@@ -100,7 +100,7 @@ var userProfileL10n = {"user_id":"0","nonce":"3dd02e420b"};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2021-08-01 11:04:56+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"pt_BR"},"Your new password has not been saved.":["Sua nova senha não foi salva."],"Hide":["Esconder"],"Show":["Mostrar"],"Confirm use of weak password":["Confirmar o uso de uma senha fraca"],"Hide password":["Ocultar senha"],"Show password":["Mostrar senha"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
 </script>
-<script type="text/javascript" src="http://webfcdigital.github.io/faberinfo/wp-admin/js/user-profile.min.js?ver=5.8" id="user-profile-js"></script>
+<script type="text/javascript" src="https://webfcdigital.github.io/faberinfo/wp-admin/js/user-profile.min.js?ver=5.8" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
